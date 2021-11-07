66281bac408b7d4c9bdd9ce7ae6f1426fad69009
Ram <ramkumarceg@gmail.com>
2021-11-06 21:23:06 -0700

    python - array find sock pairs

--- array_find_pairs.py
+++ array_find_pairs.py
@@ -1,8 +1,20 @@
 def main():
-  number_of_socks = int(raw_input().strip())
-  sock_colors = list(map(int, input().rstrip().split()))
+  number_of_socks = int(input().strip())
+  sock_colors = list(map(str, input().split(' ')))
+  sock_colors.sort()
+  colors = {}
+  total_pairs = 0
+  if(number_of_socks < 1 or number_of_socks > 100):
+    print("Number of socks has to be between 1 and 100")
   for color in sock_colors:
-    print(color)
+    if(int(color) < 1 or int(color) > 100):
+      print("Color of sock has to be between 1 and 100")
+    count = colors.get(color, 0)
+    colors[color] = count + 1
+    color_count = colors[color]
+    if(color_count > 1 and (color_count % 2 == 0)):
+      total_pairs += 1
+  print(total_pairs)
 
 if __name__ == "__main__":
   main()  
