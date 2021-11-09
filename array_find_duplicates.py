6e0266365bff6400ef8e69ee7ea331fec3942d6c
Ram <ramkumarceg@gmail.com>
2021-11-08 20:25:25 -0800

    fix array duplicates

--- array_find_duplicates.py
+++ array_find_duplicates.py
@@ -1,10 +1,13 @@
-def containsDuplicates(numbers: List[int]) -> bool:
-  numbers_set = {}
+from typing import List
+
+def main(numbers: List[int]) -> bool:
+  numbers_set = set()
   for number in numbers:
     if number in numbers_set:
       return True
     else:
       numbers_set.add(number)
-
+  return False
+  
 if __name__ == "__main__":
-  main()  
+  print(main([1,2,3]))
