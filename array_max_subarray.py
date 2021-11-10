65069581c5b91b7992c5e05b4d1acf90d01a6da4
Ram <ramkumarceg@gmail.com>
2021-11-09 20:37:23 -0800

    initial changes to find max sum

--- array_max_subarray.py
+++ array_max_subarray.py
@@ -1,4 +1,13 @@
-def main():
+from typing import List
+
+def main(numbers: List[int], k=3) -> bool:
+  max_sum = 0
+  window_sum = 0
+  window_sum = sum(numbers[:k])
+  for number in range(len(numbers) - k ):
+    window_sum = window_sum - numbers
+    if(window_sum > max_sum):
+      max_sum = window_sum
   print("max sum")
 
 if __name__ == "__main__":
