edea495415b94901adbd74a701457aede235553d
Ram <ramkumarceg@gmail.com>
2021-11-15 21:02:50 -0800

    update max subarray

--- array_max_subarray.py
+++ array_max_subarray.py
@@ -1,18 +1,17 @@
 from typing import List
 
 def main(numbers: List[int], k=3) -> bool:
-  max_sum = 0
-  window_sum = 0
+  max_sum = numbers[0]
+  sum_till_here = 0
   numbers_length = len(numbers)
   if(numbers_length == 0):
     print("Array is empty")
-  if(numbers_length < k):
-    k = numbers_length  
-  window_sum = sum(numbers[:k])
-  for number_index in range(numbers_length - k ):
-    window_sum = window_sum - numbers[number_index] + numbers[number_index + k]
-    if(window_sum > max_sum):
-      max_sum = window_sum
+  for number in numbers:
+    sum_till_here = sum_till_here + number
+    if(sum_till_here > max_sum):
+      max_sum = sum_till_here
+    if(sum_till_here < 0):
+      sum_till_here = 0
   print(max_sum)
 
 if __name__ == "__main__":
